767a79761b1b8714f74aaa361100b25ecb676c6c
arun emmidi <83764669+arunthe01@users.noreply.github.com>
2021-10-13 16:41:15 +0530

    Update and rename queue by arun emmidi.cpp to basic linked list.cpp

---
+++ basic linked list.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+class a{
+public:
+int front;
+int rear;
+int capacity;
+int size;
+int *arr;
+
+a(int cap=1){
+	front = rear=0;
+	capacity=cap;
+	arr=new int;
+	size=0;
+}
+
+void push(int n){
+
+	if(size==0){
+		arr[front]=n;
+		rear++;
+		size++;
+		return;
+	}
+	if(size==capacity-1){
+		cout<<"full";
+		return;
+	}
+	arr[rear]=n;
+	rear+=1;
+	size+=1;
+}
+void del(){
+	if(front==rear){
+		return;
+	}
+	front=front+1;
+
+}
+
+void display(){
+	int i=front;
+	while(size>i){
+		cout<<arr[i];
+		i++;
+	}
+}
+};
+
+main(){
+
+a q(5);
+q.push(7);
+q.push(9);
+q.del();
+q.display();
+
+
+}
