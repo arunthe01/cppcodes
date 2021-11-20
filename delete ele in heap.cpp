5dcfd310dfdb115344a472b007d17852a4b66e13
arun emmidi <83764669+arunthe01@users.noreply.github.com>
2021-11-20 15:54:30 +0530

    Create delete ele in heap.cpp

---
+++ delete ele in heap.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+void heapify(int arr[], int n, int i){
+	 int smallest=i;
+	 int l = i*2+1;
+	 int r = i*2+2;
+	 
+	 if(l<n && arr[smallest]>arr[l]){   
+	 	smallest=l;
+	 }
+	 
+	 if(r<n && arr[smallest]>arr[r]){
+	 	smallest=r;
+	 }
+	 
+	 if(smallest != i){
+	 	swap(arr[i],arr[smallest]);
+	 	heapify(arr,n,smallest);
+	 }
+}
+
+void deleteele(int arr[], int& n , int tbd){
+	 int ind;
+	 for(int i=0 ; i<n ; i++){
+	 	if(arr[i]==tbd){
+	 		ind=i;
+	 		break;
+		 }
+	 }
+	 
+	 arr[ind]=arr[n-1];
+	 n=n-1;
+	 heapify(arr,n,ind);
+	 
+}
+
+void printnode(int arr[],int n){
+	 for(int i =0 ; i<n ; i++){
+	 	cout<<arr[i]<<"\t";
+	 }
+}
+
+int main(){
+	int arr[]= {2,5,10,15,12,11,17};
+	
+/*         2
+	     /  \ 
+	   5     10                   
+	  / \   / \
+    15 12  11 17  
+*/
+	int n=(sizeof(arr)/sizeof(arr[0]));
+  	deleteele(arr,n,10);
+  	printnode(arr,n);
+  
+
+}
